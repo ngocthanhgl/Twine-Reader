a5b58bc11e3ce9fc96366d977d72f3dc3101f8e6
ngocthanhgl <ngocthanhgl@users.noreply.github.com>
2026-07-07 21:45:06 +0700

    fix: return Size.Unspecified for image intrinsicSize

--- shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/markdown/CoilMarkdownTransformer.kt
+++ shared/src/commonMain/kotlin/dev/sasikanth/rss/reader/markdown/CoilMarkdownTransformer.kt
@@ -22,18 +22,13 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImagePainter
 import coil3.compose.LocalPlatformContext
 import coil3.compose.rememberAsyncImagePainter
 import coil3.request.ImageRequest
@@ -67,12 +62,6 @@ object CoilMarkdownTransformer : ImageTransformer {
 
   @Composable
   override fun intrinsicSize(painter: Painter): Size {
-    var size by remember(painter) { mutableStateOf(painter.intrinsicSize) }
-    if (painter is AsyncImagePainter) {
-      val painterState = painter.state.collectAsState()
-      val intrinsicSize = painterState.value.painter?.intrinsicSize
-      intrinsicSize?.also { size = it }
-    }
-    return size
+    return Size.Unspecified
   }
 }
